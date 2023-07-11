b2385683aa80aaaec0d019c0e3a0e118b4b3e076
Cancey Dejean <canceyd@gmail.com>
2023-07-10 23:15:01 -0500

    Updated Dropdown

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar"
+import DropDownMenu from "./DropDownMenu"
 import TopRoom from "./TopRoom"
 import Footer from "./Footer"
 
-export { Navbar, Footer, TopRoom }
+export { Navbar, Footer, TopRoom, DropDownMenu }
